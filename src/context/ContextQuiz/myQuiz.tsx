e75e986f95894334211e57836a490db97dae3173
walissonsou <walissonsouza96@hotmail.com>
2022-11-27 23:50:03 -0300

    Usando os ensinamentos do useReducer e useContext

--- src/context/ContextQuiz/myQuiz.tsx
+++ src/context/ContextQuiz/myQuiz.tsx
@@ -1,9 +1,29 @@
 import { createContext } from 'react';
+import questions from '../../data/questions'
 
 interface AppContextInterface {
   name: string;
   author: string;
   url: string;
 }
+
+const STAGES = ['Start', 'Playing', 'End']
+
+const initialState = {
+  gameStage: STAGES[0],
+  questions
+}
+
+const quizReducer = (state, action) =>  { 
+  console.log(state, action)
+  
+  switch (action.type) {
+    case "CHANGE_STATE": 
+      return state
+      
+    default: 
+    return state
+    }
+  }
 
 export const myQuiz = createContext<AppContextInterface | null>(null)
