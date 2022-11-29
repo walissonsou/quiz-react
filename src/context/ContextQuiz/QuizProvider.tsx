98438013f0b6bb5cb76b2ba8558f22c46cbcace3
walissonsou <walissonsouza96@hotmail.com>
2022-11-29 10:46:08 -0300

    Organização do contexto

--- src/context/ContextQuiz/QuizProvider.tsx
+++ src/context/ContextQuiz/QuizProvider.tsx
@@ -1,9 +1,39 @@
 import { myQuiz } from "./myQuiz";
+import questions from '../../data/questions'
 
-export const ContextProvider = ({children}) => { 
+export interface AppContextInterface {
+  name: string;
+  author: string;
+  url: string;
+}
 
+// const STAGES = ['Start', 'Playing', 'End']
+  
+// const initialState = {
+//   gameStage: STAGES[0],
+//   questions
+// }
+
+export const ContextProvider = ({children}: any) => {  
+  const sampleAppContext : AppContextInterface = 
+  { 
+    name: "Using React Context in a Typescript App",
+    author: "thehappybug",
+    url: "http://www.example.com",
+}
+
+  // const quizReducer = (state, action) =>  {    
+    
+  //   switch (action.type) {
+  //     case "CHANGE_STATE": 
+  //       return state        
+  //     default: 
+  //       return state
+  //     }
+  //   }
+  
   return (
-    <myQuiz.Provider  > 
+    <myQuiz.Provider value={sampleAppContext} > 
       {children}
     </myQuiz.Provider>
   );
--- src/context/ContextQuiz/myQuiz.tsx
+++ src/context/ContextQuiz/myQuiz.tsx
@@ -1,29 +1,4 @@
 import { createContext } from 'react';
-import questions from '../../data/questions'
-
-interface AppContextInterface {
-  name: string;
-  author: string;
-  url: string;
-}
-
-const STAGES = ['Start', 'Playing', 'End']
-
-const initialState = {
-  gameStage: STAGES[0],
-  questions
-}
-
-const quizReducer = (state, action) =>  { 
-  console.log(state, action)
-  
-  switch (action.type) {
-    case "CHANGE_STATE": 
-      return state
-      
-    default: 
-    return state
-    }
-  }
+import { AppContextInterface } from './QuizProvider';
 
 export const myQuiz = createContext<AppContextInterface | null>(null)
