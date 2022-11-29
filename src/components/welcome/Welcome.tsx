c7b425b0e063885243e25d0913da85e49e96834c
walissonsou <walissonsouza96@hotmail.com>
2022-11-29 11:53:47 -0300

    Validação de type c/ função no button

--- src/components/welcome/Welcome.tsx
+++ src/components/welcome/Welcome.tsx
@@ -2,13 +2,16 @@ import React, { useContext } from "react";
 import { myQuiz } from "../../context/ContextQuiz/myQuiz";
 import Logo from "../../img/Fifa.png";
 import './Welcome.css'
+
+
 export const Welcome = () => {
 
-  const quizState = useContext(myQuiz)
-  console.log(myQuiz)
+  const [ quizState, dispatch ] = useContext(myQuiz) 
+
+
   return (
     <div id="welcome">
-      <h2> Seja bem-vido ao Quiz da Copa 2022</h2>   <button> Iniciar </button>  
+      <h2> Seja bem-vido ao Quiz da Copa 2022</h2>   <button onClick={() => dispatch({type: 'CHANGE_STATE'})}> Iniciar </button>  
       <p> Clique no botão acima para iniciar </p>
       
 
