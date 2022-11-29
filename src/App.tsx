30d95f91404495e5303a2633bc16f784d7ee4dd7
walissonsou <walissonsouza96@hotmail.com>
2022-11-29 11:51:38 -0300

    Fazendo a verificação do type para encaminhar p/ o componente correspondente

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
 import { Welcome } from './components/welcome/Welcome'
 import './App.css'
+import { Question } from './components/Questions/Question';
+import { useContext } from 'react';
+import { myQuiz } from './context/ContextQuiz/myQuiz';
 function App() {
 
+  const [ quizState, dispatch ] = useContext(myQuiz) 
+  
   return (
     <div className="App">
       <h1> Quiz da Copa </h1>
-      <Welcome />
+      { quizState.gameStage === "Start" && <Welcome /> }
+      { quizState.gameStage === "Playing" && <Question /> }
     </div>
   );
 }
