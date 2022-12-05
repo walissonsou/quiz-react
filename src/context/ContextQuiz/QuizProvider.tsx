2cbbd6c357726d122820130138f38e4e08ea1448
walissonsou <walissonsouza96@hotmail.com>
2022-12-05 11:39:10 -0300

    build: add new actions from quizReducer

--- src/context/ContextQuiz/QuizProvider.tsx
+++ src/context/ContextQuiz/QuizProvider.tsx
@@ -9,26 +9,49 @@ const STAGES = ['Start', 'Playing', 'End']
 const initialState = {
   gameStage: STAGES[0],
   questions,
+  currentQuestion: 0,
+  currentOption: 0,
 }
+
 type State =  {
   gameStage: any;
+  currentQuestion: 0,
  }
 
-type Action = { type: "CHANGE_STATE" } | { type:"FIX-STATE" }
+type Action = { type: "CHANGE_STATE" } | { type:"CHANGE_QUESTION" | { type : "REORDER_QUESTION" } }
 
-export const ContextProvider = ({children}: any) => {  
-  
-  
-  // state -> initialstate
-  // action -> ação que o usuário faz
-  const quizReducer = (state: State, action: Action) =>  {    
-    console.log(state, action)
+export const ContextProvider = ({children}: any) => {   
+
+  const quizReducer = (state: State, action: Action) =>  {        
     switch (action.type) {
       case "CHANGE_STATE": 
         return {
           ...state,
           gameStage: STAGES[1]
-        }      
+        }
+      case "REORDER_QUESTION":
+        const embaralhar = questions.sort(() => { 
+          return Math.random() - 0.5
+        })  
+      
+      return { 
+          ...state,
+          questions: embaralhar
+        }
+      case "CHANGE_QUESTION":   
+        const newQuestion = state.currentQuestion + 1
+        let endQuestions = false
+
+        if(!questions[newQuestion]){
+          endQuestions = true
+        }
+        return {
+          ...state,
+          currentQuestion: newQuestion,
+          gameStage: endQuestions ? STAGES[2] : state.gameStage
+        }    
+      case "BACK_INITIAL":
+        return initialState              
       default: 
         return state
       }
