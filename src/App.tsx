e237dde11a4db475af550305593bc7f06c4b8e9d
walissonsou <walissonsouza96@hotmail.com>
2022-12-05 11:37:38 -0300

    build: add new component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { Welcome } from './components/welcome/Welcome'
 import './App.css'
 import { Question } from './components/Questions/Question';
+
 import { useContext } from 'react';
 import { myQuiz } from './context/ContextQuiz/myQuiz';
+import { EndQuestions } from './components/EndQuestions/Endquestions';
 function App() {
 
   const [ quizState, dispatch ] = useContext(myQuiz) 
@@ -12,6 +14,7 @@ function App() {
       <h1> Quiz da Copa </h1>
       { quizState.gameStage === "Start" && <Welcome /> }
       { quizState.gameStage === "Playing" && <Question /> }
+      { quizState.gameStage === "End" && <EndQuestions /> }
     </div>
   );
 }
